bee8469dcbf0c2e252b710e67723641eb741b55c
Justin Zhu <dr_kimsora@yahoo.com>
2018-12-14 20:50:13 -0800

    Update Charge.cs

--- Actions/Charge.cs
+++ Actions/Charge.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Functions;
 
+//One of the abilities that allows the character to run to enemy target at high speed
 public class Charge : Actions {
     private float MaxDamage = 100.0f;
     private float MinDamage = 60.0f;
@@ -20,12 +21,13 @@ public class Charge : Actions {
 
     public override bool CastAction(Animator A, GameObject Weapon, Player Target)
     {
-        if (A.GetComponent<CharacterHealth>().CurrentMana < 10.0f)
+        if (A.GetComponent<CharacterHealth>().CurrentMana < 20.0f) //It utilizes mana, if not enough mana, return false and it will not cast
             return false;
         A.SetBool("Idle", false);
         GameObject Avatar = A.gameObject;
         GameObject EAvatar = Target.gameObject;
-
+        
+        //Notice that I didn't use the base class align search method because this ability is sort of unique
         if (AttackPhase && !TargetFound)
         {
             Collider[] DetectTarget = Physics.OverlapSphere(A.gameObject.transform.position, 3.0f);
@@ -64,6 +66,7 @@ public class Charge : Actions {
         return true;
     }
 
+    //Check to see if animation finish casting
     public override bool FinishCasting(Animator A, GameObject Weapon, Player Target)
     {
         if(TargetFound && (A.GetCurrentAnimatorStateInfo(1).IsName("CombatBigSwingRight") || A.GetCurrentAnimatorStateInfo(0).IsName("StopRunning")))
@@ -79,6 +82,7 @@ public class Charge : Actions {
         return false;
     }
 
+    //Callback method that use by one of the method in Weapon.cs class
     public void React(Player Target)
     {
         Target.UploadStatus(new Stunned(Stuns.Hit, Target.GetComponent<Animator>()), "Stun");
@@ -86,6 +90,7 @@ public class Charge : Actions {
         Target.GetComponent<CharacterHealth>().UpdateHealth(-Random.Range(MinDamage, MaxDamage));
     }
 
+    //Callback method that use by one of the method in Weapon.cs class
     public void TargetBlockReact(Player Target)
     {
         Target.GetComponent<Player>().UploadStatus(new Stunned(Stuns.GuardAttacked, Target.GetComponent<Animator>()), "Stun");
