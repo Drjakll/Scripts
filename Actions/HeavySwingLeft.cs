e09278920b772620eb3a0bf0984826a897ba230b
Justin Zhu <dr_kimsora@yahoo.com>
2018-12-14 20:54:06 -0800

    Update HeavySwingLeft.cs

--- Actions/HeavySwingLeft.cs
+++ Actions/HeavySwingLeft.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Functions;
 
+//Cast heavy swing attack, deals more damage than regular swing attacks
 public class HeavySwingLeft : Actions {
 
     private float MaxDamage = 150.0f;
@@ -10,12 +11,13 @@ public class HeavySwingLeft : Actions {
 
     public override bool CastAction(Animator A, GameObject Weapon, Player Target)
     {
+        //Check to see if character is facing enemy target
         if (!aim)
         {
             if (Align(A.gameObject.transform, Target.gameObject.transform))
                 aim = true;
             return false;
-        }
+        }//If aim is true, then it will check to see if it reaches within attacking range of its target
         else if (!DestinationReached)
         {
             if (!RunToTarget(A.transform, Target.transform))
@@ -28,6 +30,7 @@ public class HeavySwingLeft : Actions {
         return true;
     }
 
+    ////Callback method that use by one of the method in Weapon.cs class
     public void TargetReact(Player Target)
     {
         Target.UploadStatus(new Stunned(Stuns.Hit, Target.GetComponent<Animator>()), "Stun");
@@ -35,11 +38,13 @@ public class HeavySwingLeft : Actions {
         Target.GetComponent<CharacterHealth>().UpdateHealth(-Random.Range(MinDamage, MaxDamage));
     }
 
+    //Check to see if it's done casting its animation
     public override bool FinishCasting(Animator A, GameObject Weapon, Player Target)
     {
         return A.GetCurrentAnimatorStateInfo(1).IsName("CombatBigSwingLeft") && A.GetCurrentAnimatorStateInfo(1).normalizedTime >= 1.0f;
     }
 
+    //Callback method that use by one of the method in Weapon.cs class
     public void TargetBlockReact(Player Target)
     {
         Target.GetComponent<Player>().UploadStatus(new Stunned(Stuns.GuardAttacked, Target.GetComponent<Animator>()), "Stun");
